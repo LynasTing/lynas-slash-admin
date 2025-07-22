e4dfe606effb27a55281ab8de683cac6a185ffbc
LynasTing <LynasTing@proton.me>
2025-07-22 08:57:43 +0800

    chore: 更新了prettier和eslint配置，完成了无内容标签自闭合的功能

--- eslint.config.js
+++ eslint.config.js
@@ -1,23 +1,28 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from 'typescript-eslint'
-import { globalIgnores } from 'eslint/config'
+import js from '@eslint/js';
+import react from 'eslint-plugin-react'
+import globals from 'globals';
+import reactHooks from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
+import tseslint from 'typescript-eslint';
+import { globalIgnores } from 'eslint/config';
 
 export default tseslint.config([
   globalIgnores(['dist']),
   {
     files: ['**/*.{ts,tsx}'],
-    extends: [
-      js.configs.recommended,
-      tseslint.configs.recommended,
-      reactHooks.configs['recommended-latest'],
-      reactRefresh.configs.vite,
-    ],
+    plugins: {
+      react
+    },
+    extends: [js.configs.recommended, tseslint.configs.recommended, reactHooks.configs['recommended-latest'], reactRefresh.configs.vite],
+    rules: {
+      'react/self-closing-comp': ['error', {
+        component: true, // 组件用自闭合
+        html: true       // 原生 HTML 标签也用
+      }]
+    },
     languageOptions: {
       ecmaVersion: 2020,
-      globals: globals.browser,
-    },
-  },
-])
+      globals: globals.browser
+    }
+  }
+]);
