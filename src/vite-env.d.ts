b231e76c35ad573fd645fe196e7ad4eabe9b3b77
LynasTing <LynasTing@proton.me>
2025-07-23 19:26:41 +0800

    chore(global): 1. vite-env文件中新增了ImportMetaEnv类型声明; 2.新增了config/global.ts的全局变量配置文件

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,31 @@
 /// <reference types="vite/client" />
+
+interface ImportMetaEnv {
+  /**
+   * 默认路由
+   * Default route
+   */
+  readonly VITE_APP_DEFAULT_ROUTE: string;
+
+  /**
+   * 公共资源路径
+   * Public path
+   */
+  readonly VITE_APP_PUBLIC_PATH: string;
+
+  /**
+   * API请求基地址
+   * API base URL
+   */
+  readonly VITE_APP_API_BASE_URL: string;
+
+  /**
+   * 路由模式
+   * Router mode
+   */
+  readonly VITE_APP_ROUTER_MODE: 'frontend' | 'backend';
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
