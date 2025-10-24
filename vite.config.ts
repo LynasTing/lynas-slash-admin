87b81409020b71a748fee9c7fbef0b3129e7fa97
LynasTing <LynasTing@proton.me>
2025-10-24 10:24:18 +0800

    chore: 更新了tsconfig.json配置，并配置了路径别名符

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,16 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import tsconfigPaths from 'vite-tsconfig-paths';
+// import path from 'path';
 
 export default defineConfig({
-  plugins: [react()]
+  plugins: [react(), tsconfigPaths()],
+  server: {
+    port: 5678
+  }
+  // resolve: {
+  //   alias: {
+  //     '@': path.resolve(__dirname, 'src')
+  //   }
+  // }
 });
