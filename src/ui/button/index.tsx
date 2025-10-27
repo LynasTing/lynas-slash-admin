8f31858f222622e94966f45ee37f6ce5e1f0b0ed
LynasTing <LynasTing@proton.me>
2025-10-27 19:11:43 +0800

    feat: 新增了ui/button组件

---
+++ src/ui/button/index.tsx
@@ -0,0 +1,47 @@
+import * as React from 'react';
+import { cn } from '@/utils';
+import { Slot as SlotPrimitive } from 'radix-ui';
+import { VariantProps } from 'class-variance-authority';
+import { buttonVariants } from './styles.ts';
+
+export interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement>, VariantProps<typeof buttonVariants> {
+  asChild?: boolean;
+}
+
+/**
+ * React.forwardRef
+ * - 接收一个带 ref 参数的函数组件
+ * - 返回一个支持 ref 的 React 组件
+ *
+ * <HTMLButtonElement, ButtonProps>
+ * - ref 指向什么 DOM 元素
+ * - props 的类型
+ *
+ * (props, ref) 组件函数签名, forwardRef 会自动注入 ref
+ *
+ * @returns <ForwardRefExoticComponent> 一个带 ref 能力的组件
+ */
+const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(({ className, variant, size, asChild = false, ...props }, ref) => {
+  const Comp = asChild ? SlotPrimitive.Slot : 'button';
+  return (
+    <Comp
+      className={cn(
+        buttonVariants({
+          variant,
+          size,
+          className
+        })
+      )}
+      ref={ref}
+      {...props}
+    />
+  );
+});
+
+/**
+ * 命名组件
+ * - 用于 DevTools 调试 + 提示消息更友好，不影响功能和类型。
+ */
+Button.displayName = 'Button';
+
+export { Button };
---
+++ src/ui/button/styles.ts
@@ -0,0 +1,48 @@
+import { cva } from 'class-variance-authority';
+
+export const buttonVariants = cva(
+  [
+    // layout & spacing 布局和间距
+    'inline-flex justify-center items-center gap-2 whitespace-nowrap',
+    // shape & typography 形状和排版
+    'rounded-md text-sm font-medium',
+    // interaction 交互
+    'transition-colors focus-visible:outline-hidden focus-visible:ring-1 focus-visible:ring-ring',
+    // disabled & SVG child 禁用和 SVG 子元素
+    'disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg]:size-4 [&_svg]:shrink-0'
+  ],
+  {
+    variants: {
+      variant: {
+        // 默认
+        default: 'bg-primary text-primary-foreground shadow hover:bg-primary/90',
+        // 危险
+        destructive: 'bg-destructive text-destructive-foreground shadow-sm hover:bg-destructive/90',
+        // 轮廓
+        outline: 'border border-input bg-background shadow-sm hover:bg-accent hover:text-accent-foreground',
+        // 次要
+        secondary: 'bg-secondary text-secondary-foreground shadow-sm hover:bg-secondary/80',
+        // 幽灵
+        ghost: 'hover:bg-accent hover:text-accent-foreground',
+        // 文本链接
+        link: 'text-primary underline-offset-4 hover:underline',
+        // 黑白对比
+        contrast: 'bg-black text-white dark:bg-white dark:text-black hover:bg-black/80 dark:hover:bg-white/80'
+      },
+      size: {
+        // 默认
+        default: 'h-9 px-4 py-2',
+        // 小
+        sm: 'h-8 rounded-md px-3 text-xs',
+        // 大
+        lg: 'h-10 rounded-md px-8',
+        // 图标
+        icon: 'w-9 h-9  cursor-pointer'
+      }
+    },
+    defaultVariants: {
+      variant: 'default',
+      size: 'default'
+    }
+  }
+);
