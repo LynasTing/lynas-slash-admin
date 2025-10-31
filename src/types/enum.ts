c86280eba162787ca0189b8a1e76f92b447031b8
LynasTing <LynasTing@proton.me>
2025-10-31 14:02:14 +0800

    feat: 新增了 types/enum 中的存储类型和语言类型的枚举对象

---
+++ src/types/enum.ts
@@ -0,0 +1,19 @@
+/**
+ * 存储类型
+ * Storage type
+ */
+export enum StorageEnum {
+  UserInfo = 'userInfo',
+  UserToken = 'userToken',
+  Settings = 'settings',
+  I18N = 'i18nextLng'
+}
+
+/**
+ * 语言类型
+ * Language type
+ */
+export enum LocalEnum {
+  en_US = 'en_US',
+  zh_CN = 'zh_CN'
+}
