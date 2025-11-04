8505aac82270e3fc5db4ccfb99fedf51d621479a
LynasTing <LynasTing@proton.me>
2025-11-04 09:36:36 +0800

    chore: vite.config.ts 中 加载了 vanillaExtractPlugin 插件

--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,17 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import tsconfigPaths from 'vite-tsconfig-paths';
 import tailwindcss from '@tailwindcss/vite';
+import { vanillaExtractPlugin } from '@vanilla-extract/vite-plugin';
 
 export default defineConfig({
-  plugins: [react(), tailwindcss(), tsconfigPaths()],
+  plugins: [
+    react(),
+    tailwindcss(),
+    tsconfigPaths(),
+    vanillaExtractPlugin({
+      identifiers: ({ debugId }) => `${debugId}`
+    })
+  ],
   server: {
     port: 5678
   }
