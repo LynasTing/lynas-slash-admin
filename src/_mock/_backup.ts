b2a0d546ad05ecc4510bd9ebf1f5f0410ca2b904
LynasTing <LynasTing@proton.me>
2025-11-06 14:22:33 +0800

    feat: _mock/_backup.ts 文件新增了 DB_USER 内容

---
+++ src/_mock/_backup.ts
@@ -0,0 +1,8 @@
+import type { User } from '#/entity';
+import { faker } from '@faker-js/faker';
+
+export const DB_USER: User[] = [
+  { id: 'user_admin_id', username: 'admin', password: 'demo1234', avatar: faker.image.avatarGitHub(), email: 'admin@slash.com' },
+  { id: 'user_test_id', username: 'test', password: 'demo1234', avatar: faker.image.avatarGitHub(), email: 'test@slash.com' },
+  { id: 'user_guest_id', username: 'guest', password: 'demo1234', avatar: faker.image.avatarGitHub(), email: 'guest@slash.com' }
+];
