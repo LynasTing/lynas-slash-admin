739e9a99874d816da85c9835a11420c846d074c0
LynasTing <LynasTing@proton.me>
2025-11-18 18:03:48 +0800

    feat(api): 新增了 api/services/auth 登录接口的定义

--- src/api/services/auth.ts
+++ src/api/services/auth.ts
@@ -1,4 +1,27 @@
+import { UserToken, User } from '#/entity';
+import { apiClient } from '@/utils';
+
+export enum UserApi {
+  SignIn = '/auth/signin'
+}
+
 export interface SignInRequest {
   username: string;
   password: string;
 }
+
+/**
+ * 登录-响应
+ */
+export type SignInResponse = UserToken & { user: User };
+
+/**
+ * 登录
+ */
+const signinApi = (data: SignInRequest) =>
+  apiClient.post<SignInResponse>({
+    url: UserApi.SignIn,
+    data
+  });
+
+export { signinApi };
