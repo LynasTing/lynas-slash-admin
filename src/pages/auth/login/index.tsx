bd3de198000a213457d81a01eac93e334c685410
LynasTing <LynasTing@proton.me>
2025-11-21 14:55:45 +0800

    feat(auth): 完成了基础的账号密码登录功能

--- src/pages/auth/login/index.tsx
+++ src/pages/auth/login/index.tsx
@@ -1,5 +1,36 @@
-function Login() {
-  return <div>登录</div>;
+import Logo from '@/components/logo';
+import PlaceholderImg from '@/assets/images/background/placeholder.svg';
+import { GLOBAL_CONFIG } from '@/config/global';
+import { LoginProvider } from './providers/context';
+import LoginForm from './login-form';
+import LocalePicker from '@/components/locale-picker';
+
+function LoginPage() {
+  return (
+    <div className="relative grid min-h-svh lg:grid-cols-2">
+      <div className="flex flex-col gap-4 p-6 md:p-10">
+        <div className="flex justify-center gap-2 md:justify-start">
+          <div className="flex items-center gap-2 font-medium cursor-pointer">
+            <Logo size={28} />
+            <h1>{GLOBAL_CONFIG.appName}</h1>
+          </div>
+        </div>
+        <div className="flex-1 flex justify-center items-center">
+          <div className="w-full max-w-xs">
+            <LoginProvider>
+              <LoginForm />
+            </LoginProvider>
+          </div>
+        </div>
+      </div>
+      <div className="relative hidden lg:block">
+        <img src={PlaceholderImg} alt="placeholder img" className="absolute inset-0 w-full h-full object-cover" />
+      </div>
+      <div className="absolute right-2 top-0 flex flex-row">
+        <LocalePicker />
+      </div>
+    </div>
+  );
 }
 
-export default Login;
+export default LoginPage;
--- src/pages/auth/login/login-form.tsx
+++ src/pages/auth/login/login-form.tsx
@@ -1,40 +1,65 @@
-import * as React from 'react';
+/**
+ * @description 账号 + 密码 登录表单
+ * @author LynasTing
+ * @date 2025-11-21
+ */
+
 import { cn } from '@/utils';
-// import { useTranslation } from "react-i18next";
-// import { useState } from "react";
-// import {  LoginProvider } from './providers/context'
-// import type { LoginStateEnum } from './providers/declaration';
-// import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from '@/ui/form';
-import { Form, FormField, FormItem, FormLabel, FormControl } from '@/ui/form';
+import { Form, FormField, FormItem, FormLabel, FormControl, FormMessage } from '@/ui/form';
 import { Input } from '@/ui/input';
 import { useForm } from 'react-hook-form';
 import type { SignInRequest } from '@/api/services/auth';
-// import { DB_USER } from '@/_mock/_backup';
 import { Button } from '@/ui/button';
 import { useTranslation } from 'react-i18next';
+import { useState, type ComponentPropsWithoutRef } from 'react';
+import { useSignIn } from '@/store/user';
+import { toast } from 'sonner';
+import { useLoginStateContext } from './providers/declaration';
+import { LoginStateEnum } from './providers/declaration';
+import { Loader2 } from 'lucide-react';
+import { useNavigate } from 'react-router';
+import { GLOBAL_CONFIG } from '@/config/global';
 
 /**
  * ComponentPropsWithoutRef React 内置的类型工具
  * - 获取某个组件的 props 类型，但去掉 ref 属性
  */
-function LoginForm({ className, ...props }: React.ComponentPropsWithoutRef<'form'>) {
+function LoginForm({ className, ...props }: ComponentPropsWithoutRef<'form'>) {
   const { t } = useTranslation();
+  const [loading, setLoading] = useState(false);
+  const navigate = useNavigate();
+
+  const { loginState } = useLoginStateContext();
+  const signIn = useSignIn();
 
   const form = useForm<SignInRequest>({
     defaultValues: {
-      // username: DB_USER[0].username,
-      // password: DB_USER[0].password
       username: '',
       password: ''
     }
   });
 
-  // const finish = (value: )
+  if (loginState !== LoginStateEnum.LOGIN) {
+    return null;
+  }
+
+  const finish = async (values: SignInRequest) => {
+    setLoading(true);
+    try {
+      await signIn(values);
+      navigate(GLOBAL_CONFIG.defaultRoute, { replace: true });
+      toast.success(t('auth.loginSuccessTitle'), {
+        closeButton: true
+      });
+    } finally {
+      setLoading(false);
+    }
+  };
 
   return (
     <div className={cn('flex flex-col gap-6', className)}>
       <Form {...form} {...props}>
-        <form className="space-y-4">
+        <form className="space-y-4" onSubmit={form.handleSubmit(finish)}>
           <div className="flex flex-col items-center gap-2 text-center">
             <h1 className="text-2xl font-bold">{t('auth.signInFormTitle')}</h1>
             <p className="text-balance text-sm text-muted-foreground">{t('auth.signInFormDescription')}</p>
@@ -50,6 +75,7 @@ function LoginForm({ className, ...props }: React.ComponentPropsWithoutRef<'form
                 <FormControl>
                   <Input placeholder={t('auth.accountPlaceholder')} {...field} />
                 </FormControl>
+                <FormMessage />
               </FormItem>
             )}
           />
@@ -64,12 +90,14 @@ function LoginForm({ className, ...props }: React.ComponentPropsWithoutRef<'form
                 <FormControl>
                   <Input type="password" placeholder={t('auth.passwordPlaceholder')} {...field} suppressHydrationWarning />
                 </FormControl>
+                <FormMessage />
               </FormItem>
             )}
           />
 
           {/* 登录按钮 */}
           <Button type="submit" className="w-full">
+            {loading && <Loader2 className="mr-2 animate-spin" />}
             {t('auth.loginButton')}
           </Button>
         </form>
