e77d1c1cdc8f4ff401e2a1dc421d6ac52acd6839
LynasTing <LynasTing@proton.me>
2025-11-21 14:56:44 +0800

    feat(pinia): 新增了 store/user 模块，包含用户信息存储及登录接口调用功能

---
+++ src/store/user.ts
@@ -0,0 +1,75 @@
+import { create } from 'zustand';
+import { persist, createJSONStorage } from 'zustand/middleware';
+import type { User, UserToken } from '#/entity';
+import { StorageEnum } from '#/enum';
+import { useMutation } from '@tanstack/react-query';
+import { signinApi, type SignInRequest } from '@/api/services/auth';
+import { toast } from 'sonner';
+
+type UserStore = {
+  userInfo: Partial<User>;
+  userToken: UserToken;
+  actions: {
+    setUserInfo: (userInfo: Partial<User>) => void;
+    setUserToken: (userToken: UserToken) => void;
+  };
+};
+
+const useUserStore = create<UserStore>()(
+  persist(
+    set => ({
+      userInfo: {},
+      userToken: {},
+      actions: {
+        setUserInfo: userInfo => set({ userInfo }),
+        setUserToken: userToken => set({ userToken })
+      }
+    }),
+    {
+      name: StorageEnum.User,
+      storage: createJSONStorage(() => localStorage),
+      partialize: state => ({
+        [StorageEnum.UserInfo]: state.userInfo,
+        [StorageEnum.UserToken]: state.userToken
+      })
+    }
+  )
+);
+
+export const useUserActions = () => useUserStore(state => state.actions);
+
+export const useSignIn = () => {
+  const { setUserToken, setUserInfo } = useUserActions();
+
+  /**
+   * useMutation 专门用来做 会改变服务器状态的操作（POST / PUT / DELETE…）
+   * - 它不会缓存数据（不像 useQuery 会缓存 GET 请求）
+   *
+   * 返回一个对象，里面主要包含：
+   * - mutate(data, options) — 触发 mutation，是异步但回调风格
+   * - mutateAsync(data, options) — 返回 Promise，方便 async/await
+   * - status — idle | loading | success | error
+   * - data — mutation 返回的成功数据
+   * - error — 请求失败返回的错误对象
+   */
+  const signInMutation = useMutation({
+    mutationFn: signinApi
+  });
+
+  const signIn = async (data: SignInRequest) => {
+    try {
+      const r = await signInMutation.mutateAsync(data);
+      const { user, accessToken, refreshToken } = r;
+
+      setUserToken({ accessToken, refreshToken });
+      setUserInfo(user);
+    } catch (err) {
+      toast.error(err.message, {
+        position: 'top-center'
+      });
+      throw err;
+    }
+  };
+
+  return signIn;
+};
