4db6d5c0f1ab2fc39cc51d511799ac708cc7631c
LynasTing <LynasTing@proton.me>
2025-11-28 08:59:20 +0800

    feat(route): App 页面新增了 QueryClientProvider 和 ThemeProvider 及 Toast 组件

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,21 @@
 import type { ReactNode } from 'react';
+import { ThemeProvider } from '@/theme/provider';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import Toast from '@/components/toast';
 
 interface IProps {
   children?: ReactNode;
 }
 
 function App({ children }: IProps) {
-  return <>{children}</>;
+  return (
+    <QueryClientProvider client={new QueryClient()}>
+      <ThemeProvider>
+        <Toast />
+        {children}
+      </ThemeProvider>
+    </QueryClientProvider>
+  );
 }
 
 export default App;
