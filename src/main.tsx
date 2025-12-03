efe40bdc16badceaf671ac413176e07700bcd04e
LynasTing <LynasTing@proton.me>
2025-12-03 14:50:42 +0800

    feat(main): main.ts 新增 msw 注册

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,24 @@
 import '@/assets/style/global.css';
+import '@/theme/theme.css';
 import '@/locales/i18n';
+import { worker } from './_mock';
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { createBrowserRouter, Outlet, RouterProvider } from 'react-router';
 import App from './App.tsx';
-import { routersSections } from './router/sections/index.tsx';
+import { routersSections } from './router';
 import { registerLocalIcons } from '@/components/icon';
+import { urlJoin } from '@/utils';
+import { GLOBAL_CONFIG } from '@/config/global';
 
 registerLocalIcons();
+
+await worker.start({
+  onUnhandledRequest: 'bypass',
+  serviceWorker: {
+    url: urlJoin(GLOBAL_CONFIG.publicPath, 'mockServiceWorker.js')
+  }
+});
 
 const router = createBrowserRouter([
   {
