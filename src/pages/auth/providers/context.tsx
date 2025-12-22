e4e3a2a2100a98dc3d1ac6d0d7b6cb8181874668
LynasTing <LynasTing@proton.me>
2025-12-22 08:59:12 +0800

    style(auth): 删除 providers 文件中的空白行

--- src/pages/auth/providers/context.tsx
+++ src/pages/auth/providers/context.tsx
@@ -1,6 +1,5 @@
 import * as React from 'react';
 import { type PropsWithChildren } from 'react';
-
 import { type LoginStateContextType, LoginStateEnum, LoginStateContext } from './declaration';
 
 export function LoginProvider({ children }: PropsWithChildren) {
