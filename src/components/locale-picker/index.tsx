a4eb876dd48c7ec36913de56a70060e23234b431
LynasTing <LynasTing@proton.me>
2026-01-06 09:30:34 +0800

    feat(components): update locale-picker import Button way.

--- src/components/locale-picker/index.tsx
+++ src/components/locale-picker/index.tsx
@@ -1,7 +1,7 @@
 import useLocale, { LANGUAGE_MAP } from '@/locales/use-locale';
 import { Icon } from '@/components/icon';
 import { DropdownMenu, DropdownMenuTrigger, DropdownMenuContent, DropdownMenuItem } from '@/ui/dropdowm-menu';
-import { Button } from '@/ui/button';
+import Button from '@/ui/button';
 
 export default function LocalePicker() {
   const { locale, setLocale } = useLocale();
