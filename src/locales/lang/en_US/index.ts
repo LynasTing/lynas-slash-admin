66aba0020602736f3855548bdb5f103b56167a0c
LynasTing <LynasTing@proton.me>
2026-01-11 14:55:43 +0800

    feat(locales): feat en and zh language config.

--- src/locales/lang/en_US/index.ts
+++ src/locales/lang/en_US/index.ts
@@ -1,7 +1,9 @@
 import auth from './jsons/auth.json';
 import sys from './jsons/sys.json';
+import common from './jsons/common.json';
 
 export default {
   ...auth,
-  ...sys
+  ...sys,
+  ...common
 };
