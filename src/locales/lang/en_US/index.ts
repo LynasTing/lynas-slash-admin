1c7ac5f9b248dd9b644c682de54e706e64e36abe
LynasTing <LynasTing@proton.me>
2026-04-03 09:14:16 +0800

    feat(i18n): add dashboard keys and  merge locale modules.
    - add totalTasks and totalIncome dashboard xx.json.
    - import and merge dashboard.json into main locale export.

--- src/locales/lang/en_US/index.ts
+++ src/locales/lang/en_US/index.ts
@@ -1,9 +1,11 @@
 import auth from './jsons/auth.json';
 import sys from './jsons/sys.json';
 import common from './jsons/common.json';
+import dashboard from './jsons/dashboard.json';
 
 export default {
   ...auth,
   ...sys,
-  ...common
+  ...common,
+  ...dashboard
 };
