151e94cb69f9110cebb7e1a7c0a746c296a18d71
LynasTing <LynasTing@proton.me>
2026-04-24 09:01:15 +0800

    chore(i18n): update multiple locale JSON files for translations.

--- src/locales/lang/zh_CN/index.ts
+++ src/locales/lang/zh_CN/index.ts
@@ -2,10 +2,12 @@ import auth from './jsons/auth.json';
 import sys from './jsons/sys.json';
 import common from './jsons/common.json';
 import dashboard from './jsons/dashboard.json';
+import ui from './jsons/ui.json';
 
 export default {
   ...auth,
   ...sys,
   ...common,
-  ...dashboard
+  ...dashboard,
+  ...ui
 };
