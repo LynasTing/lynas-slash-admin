fab8864c187cb8ab743e74fe267628ff47d25911
LynasTing <LynasTing@proton.me>
2026-05-17 10:56:52 +0800

    feat(utils): add number formatting helpers .
    - add removeTrailingZeros
    - add formatBytes

---
+++ src/utils/format-number.ts
@@ -0,0 +1,30 @@
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-ignore
+import numeral from 'numeral';
+
+type InputValue = string | number | null | undefined;
+
+/**
+ * 移除小数点后的 0
+ * Remove trailing zeros after the decimal point
+ *
+ * @param value - 输入值
+ * @param target - 小数点后的目标值
+ *
+ * @example 1.00 -> 1
+ */
+export function removeTrailingZeros(value: string, target = '.00') {
+  return value.includes(target) ? value.replace(target, '') : value;
+}
+
+/**
+ * 格式化字节
+ * Format bytes
+ *
+ * @param number - 输入值
+ *
+ * @example 1024 -> 1.00 KB
+ */
+export function formatBytes(number: InputValue) {
+  return removeTrailingZeros(number ? numeral(number).format('0.00 b') : '');
+}
