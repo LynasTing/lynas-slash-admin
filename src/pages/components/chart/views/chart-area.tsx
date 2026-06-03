a90b5227c947c921a24d35bcc3918f52fe1c62f7
LynasTing <LynasTing@proton.me>
2026-06-03 14:14:31 +0800

    feat(chart): add area chart component

---
+++ src/pages/components/chart/views/chart-area.tsx
@@ -0,0 +1,35 @@
+import { Chart, useChart } from '@/components/chart';
+
+const series = [
+  {
+    name: 'series1',
+    data: Array.from({ length: 7 }).map(() => Math.floor(Math.random() * 1000))
+  },
+  {
+    name: 'series2',
+    data: Array.from({ length: 7 }).map(() => Math.floor(Math.random() * 1000))
+  }
+];
+
+export default function ChartArea() {
+  const chartOptions = useChart({
+    xaxis: {
+      type: 'datetime',
+      categories: [
+        '2018-09-19T00:00:00.000Z',
+        '2018-09-19T01:30:00.000Z',
+        '2018-09-19T02:30:00.000Z',
+        '2018-09-19T03:30:00.000Z',
+        '2018-09-19T04:30:00.000Z',
+        '2018-09-19T05:30:00.000Z',
+        '2018-09-19T06:30:00.000Z'
+      ]
+    },
+    tooltip: {
+      x: {
+        format: 'dd/MM/yy HH:mm'
+      }
+    }
+  });
+  return <Chart type="area" series={series} options={chartOptions} height={320} />;
+}
