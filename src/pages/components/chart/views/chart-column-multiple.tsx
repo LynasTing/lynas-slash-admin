0a05d3c786f14e49e1f45b4059ae59808fbd8b42
LynasTing <LynasTing@proton.me>
2026-06-03 14:14:55 +0800

    feat(chart): add multiple column chart component

---
+++ src/pages/components/chart/views/chart-column-multiple.tsx
@@ -0,0 +1,33 @@
+import { Chart, useChart } from '@/components/chart';
+
+const series = [
+  {
+    name: 'Net Profit',
+    data: Array.from({ length: 9 }).map(() => Math.floor(Math.random() * 300) + 1)
+  },
+  {
+    name: 'Revenue',
+    data: Array.from({ length: 9 }).map(() => Math.floor(Math.random() * 300) + 1)
+  }
+];
+
+export default function ChartColumnMultiple() {
+  const chartOptions = useChart({
+    stroke: {
+      show: true,
+      width: 2,
+      colors: ['transparent']
+    },
+    xaxis: {
+      categories: ['Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct']
+    },
+    tooltip: {
+      y: {
+        formatter: (value: number) => `$${value} thousands`
+      }
+    },
+    plotOptions: { bar: { columnWidth: '36%' } }
+  });
+
+  return <Chart type="bar" series={series} options={chartOptions} height={320} />;
+}
