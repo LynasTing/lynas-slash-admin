57999048dab330f6e281103a0cbbfcfa202ccd70
LynasTing <LynasTing@proton.me>
2026-06-20 20:30:00 +0800

    style(nav): fix vertical group class ordering

--- src/components/nav/vertical/nav-group.tsx
+++ src/components/nav/vertical/nav-group.tsx
@@ -34,20 +34,20 @@ function Group({ name, open, onClick }: GroupProps) {
     name && (
       <div
         className={cn(
-          'group w-full inline-flex items-center justify-start relative gap-2 cursor-pointer pt-4 pr-2 pb-2 pl-3 transition-all duration-300 ease-in-out',
-          'hover: pl-4'
+          'group relative inline-flex w-full cursor-pointer items-center justify-start gap-2 pt-4 pr-2 pb-2 pl-3 transition-all duration-300 ease-in-out',
+          'hover:pl-4'
         )}
         onClick={() => onClick(!open)}>
         <Icon
           icon="eva:arrow-ios-forward-fill"
           className={cn(
-            'absolute left-[-4px] h-4 w-4 inline-flex shrink-0 transition-all duration-300 ease-in-out", "opacity-0 group-hover:opacity-100',
+            'absolute left-[-4px] inline-flex h-4 w-4 shrink-0 opacity-0 transition-all duration-300 ease-in-out group-hover:opacity-100',
             {
               'rotate-90': open
             }
           )}
         />
-        <span className={cn('text-xs font-medium transition-all duration-300 ease-in-out text-text-disabled', 'hover:text-text-primary')}>
+        <span className={cn('text-xs font-medium text-text-disabled transition-all duration-300 ease-in-out', 'hover:text-text-primary')}>
           {t(name)}
         </span>
       </div>
@@ -64,7 +64,7 @@ export function NavVerticalGroup({ name, items }: NavGroupProps) {
         <Group name={name} open={open} onClick={toggleOpen} />
       </CollapsibleTrigger>
       <CollapsibleContent>
-        <ul className="flex flex-col gap-1 w-full">
+        <ul className="flex w-full flex-col gap-1">
           {items.map((item, idx) => (
             <NavVerticalList key={item.title || idx} data={item} depth={1} />
           ))}
