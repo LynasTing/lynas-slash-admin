4eea767e184c427d42c1060f5824c1f9172c85b3
LynasTing <LynasTing@proton.me>
2026-07-23 09:05:17 +0800

    feat(app): set document title and favicon

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,9 @@ import { ThemeProvider } from '@/theme/provider';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import Toast from '@/components/toast';
 import { MotionLazy } from './components/animate/motion-lazy';
+import { HelmetProvider, Helmet } from 'react-helmet-async';
+import LogoIMG from '@/assets/icons/ic-logo-badge.svg';
+import { GLOBAL_CONFIG } from './config/global';
 
 if (import.meta.env.DEV) {
   import('react-scan').then(({ scan }) => {
@@ -21,12 +24,18 @@ interface IProps {
 
 function App({ children }: IProps) {
   return (
-    <QueryClientProvider client={new QueryClient()}>
-      <ThemeProvider>
-        <Toast />
-        <MotionLazy>{children}</MotionLazy>
-      </ThemeProvider>
-    </QueryClientProvider>
+    <HelmetProvider>
+      <QueryClientProvider client={new QueryClient()}>
+        <ThemeProvider>
+          <Helmet>
+            <title>{GLOBAL_CONFIG.appName}</title>
+            <link rel="icon" href={LogoIMG} />
+          </Helmet>
+          <Toast />
+          <MotionLazy>{children}</MotionLazy>
+        </ThemeProvider>
+      </QueryClientProvider>
+    </HelmetProvider>
   );
 }
 
